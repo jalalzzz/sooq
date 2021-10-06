3621b4bcba06946d17cc1b6b4e03e859144255b2
jalal <71690867+jalalzzz@users.noreply.github.com>
2021-10-06 10:56:11 +0300

    Signed-off-by: jalal <71690867+jalalzzz@users.noreply.github.com> first create button

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainView}>
+      <View style={styles.titleText}>
+        <Text style={styles.tilteText2}>شركة ابو عواد لتجارة الفواكه</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={onPress}
+        >
+          <Text style={styles.textButton}>إستلام </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={onPress}
+        >
+          <Text style={styles.textButton}>بيع</Text>
+        </TouchableOpacity>
+
+
+      </View>
     </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 7,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    flexDirection:"row",
   },
+  titleText: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center',
+    marginTop:30,
+    backgroundColor: '#ff0000',
+
+
+  },
+  mainView: {
+    flex: 1,
+    backgroundColor: '#ffff00',
+    width: '100%', 
+    height: '100%'
+  },
+  tilteText2: {
+    textShadowColor: 'red',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#000000',
+    fontSize: 22,
+    fontStyle: 'italic',
+    textDecorationLine: 'underline',
+   
+  },
+  button: {
+
+   
+
+    backgroundColor: "#cccccc",
+    padding: 10,
+    width:'30%',
+    height:100,
+    borderRadius:15,
+
+
+  },textButton:{
+    marginVertical:20,
+    textAlign:"center",
+    fontSize: 22,
+  }
 });
